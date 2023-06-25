cbbe1949bb423e9bda98029b891d0405c12bd7bb
Lyssenko Alexandr <lyssenko.alex@gmail.com>
2023-06-25 18:19:57 +0600

    init

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,16 @@
-import cors from "cors";
-import express from "express";
-import logger from "jet-logger";
-import itemController from "./controllers/items.controller";
-import suppliersController from "./controllers/suppliers.controller";
-import usersController from "./controllers/users.controller";
-import actionsController from "./controllers/actions.controller";
-import suppliersStoragesController from './controllers/suppliersStorages.controller'
-import storagesController from './controllers/storages.controller'
-import itemsCategoryController from "./controllers/ItemsCategory.controller";
-import itemsSubcategoryController from "./controllers/ItemsSubcategory.controller";
-import locationController from "./controllers/location.controller";
+import express, { Request, Response } from 'express'
 
-const run = async () => {
-  app.listen(PORT, () => {
-    logger.info(`Server is running on http://localhost:${PORT}`);
-  });
-};
+const app = express()
+const port = process.env.PORT || 8080
 
-const app = express();
-const PORT = 8000;
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel')
+})
 
-app.use(express.json());
-app.use(express.static("public"));
-app.use(cors());
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓')
+})
 
-app.use("/items", itemController);
-app.use("/suppliers", suppliersController);
-app.use("/users", usersController);
-app.use("/supply", actionsController);
-app.use("/suppliers_controllers", suppliersStoragesController);
-app.use("/storages", storagesController);
-app.use("/items_category", itemsCategoryController);
-app.use("/items_subcategory", itemsSubcategoryController);
-app.use("/location", locationController);
-
-
-// run().catch(logger.err);
-
-app.listen(PORT, () => {
-  return console.log(`Server is listening on ${PORT}`)
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`)
 })
